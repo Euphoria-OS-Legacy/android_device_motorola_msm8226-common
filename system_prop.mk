8ddb658dbd352902f6564f09b228fb1cb0077bf9
Alberto97 <albertop2197@gmail.com>
2015-01-20 22:38:32 +0100

    Set awesome as Default
    Change-Id: Ia75d89d87683159724789cc9b6fff62ae89bb997
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -62,6 +62,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.service_mask=0x5 \
     persist.gps.qc_nlp_in_use=1 \
     persist.loc.nlp_name=com.qualcomm.services.location
+    
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.media.use-awesome=true
 
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
